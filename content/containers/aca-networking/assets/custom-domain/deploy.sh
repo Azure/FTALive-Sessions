a3d0cf368cd84503d10df83b656793ba6b6ca088
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2023-01-30 15:05:26 +1100

    ACA Networking - added custom domain deployment example

--- content/containers/aca-networking/assets/custom-domain/deploy.sh
+++ content/containers/aca-networking/assets/custom-domain/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 LOCATION='australiaeast'
-PREFIX='internal'
+PREFIX='custom-dns'
 RG_NAME="${PREFIX}-aca-rg"
 SSH_KEY=`cat ~/.ssh/id_rsa.pub`
+
+# source 'CERTIFICATE_PASSWORD' from .env file
+source ./.env
 
 # create resource group
 az group create --location $LOCATION --name $RG_NAME
@@ -15,8 +18,8 @@ az deployment group create \
 	--template-file ./main.bicep \
 	--parameters location=$LOCATION \
 	--parameters prefix=$PREFIX \
-	--parameters publicSshKey="$SSH_KEY" \
-	--parameters isZoneRedundant='true'
+	--parameters certificatePassword=$CERTIFICATE_PASSWORD \
+	--parameters certificate="$(cat ./certs/star.kainiindustries.net.bundle.pfx | base64)"
 
 # get deployment template outputs
 APP_FQDN=`az deployment group show --resource-group $RG_NAME --name 'infra-deployment' --query properties.outputs.appFqdn.value --output tsv`
@@ -25,19 +28,7 @@ VM_ID=`az deployment group show --resource-group $RG_NAME --name 'infra-deployme
 
 echo "APP_FQDN: https://$APP_FQDN"
 
-# open ssh tunnel to VM in the same vnet as the ACA environment
-az network bastion ssh \
-	--name $BASTION_HOST_NAME \
-	--resource-group $RG_NAME \
-	--target-resource-id $VM_ID \
-	--auth-type ssh-key \
-	--username 'localadmin' \
-	--ssh-key '~/.ssh/id_rsa'
 
-az network bastion tunnel --name $BASTION_HOST_NAME --resource-group $RG_NAME --target-resource-id $VM_ID --resource-port 22 --port 50022
 
-# run these commands in a separate shell
-#
-# $ ssh localadmin@127.0.0.1 -p 50022
-# $ nslookup $APP_FQDN
-# $ curl https://$APP_FQDN
+
+
