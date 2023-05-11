70b2942d2775f567d9222615e8885c03ea87a5d0
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2023-05-11 11:11:51 +1000

    added more AKS Networking example environment scripts

--- content/containers/aks-operations/assets/goldilocks.sh
+++ content/containers/aks-operations/assets/goldilocks.sh
@@ -1,4 +1,53 @@
 helm repo add fairwinds-stable https://charts.fairwinds.com/stable
 helm install --name goldilocks --namespace goldilocks --set installVPA=true fairwinds-stable/goldilocks
 
+# create namespace
+kubectl create ns httpbin
+
+# label namespace to allow Goldilocaks to access it
+kubectl label ns httpbin goldilocks.fairwinds.com/enabled=true
+
+# deploy sample application
+cat <<EOF | kubectl apply -n httpbin -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: httpbin
+  labels:
+    app: httpbin
+spec:
+  ports:
+  - name: http
+    port: 8000
+    targetPort: 80
+  selector:
+    app: httpbin
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: httpbin
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: httpbin
+      version: v1
+  template:
+    metadata:
+      labels:
+        app: httpbin
+        version: v1
+    spec:
+      containers:
+      - image: docker.io/kennethreitz/httpbin
+        imagePullPolicy: IfNotPresent
+        name: httpbin
+        ports:
+        - containerPort: 80
+EOF
+
+# label namespaces
+
+
 kubectl -n goldilocks port-forward svc/goldilocks-dashboard 8080:80
